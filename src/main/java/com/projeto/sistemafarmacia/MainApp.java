52e93e362f3920c3d825fa2c7671432f87a01770
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2021-12-04 23:49:11 -0300

    Adicionando Icons

--- src/main/java/com/projeto/sistemafarmacia/MainApp.java
+++ src/main/java/com/projeto/sistemafarmacia/MainApp.java
@@ -3,6 +3,7 @@ package com.projeto.sistemafarmacia;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,13 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("fxml/ViewLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("Img/Icon_User_Main.png")));
+        stage.setResizable(false);
+        stage.setTitle("Login!");
         stage.setScene(scene);
         stage.show();
+        
+       
     }
 
     public static void main(String[] args) {
