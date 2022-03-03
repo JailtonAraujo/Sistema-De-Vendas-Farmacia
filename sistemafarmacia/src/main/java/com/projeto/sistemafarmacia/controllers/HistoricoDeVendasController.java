2e9caa4125bbe58c67e277eb5827cff2408f7c64
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-03-03 00:17:49 -0300

    update

--- sistemafarmacia/src/main/java/com/projeto/sistemafarmacia/controllers/HistoricoDeVendasController.java
+++ sistemafarmacia/src/main/java/com/projeto/sistemafarmacia/controllers/HistoricoDeVendasController.java
@@ -3,10 +3,24 @@ package com.projeto.sistemafarmacia.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 public class HistoricoDeVendasController implements Initializable{
 
+	@FXML
+    private JFXButton btnSair;
+
+    @FXML
+    void actionSair(ActionEvent event) {
+    	Stage stage = (Stage) btnSair.getScene().getWindow();
+		stage.close();
+    }
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
